95cd7d0f4c53a71b8ea000fe8cff404fd2066379
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-11-18 08:48:19 +0900

    basic graphql server

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+const fs = require('fs');
+const { GraphQLScalarType } = require('graphql');
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs = `
-  type Query {
-    totalPhotos: Int!
-  }
-`;
+const typeDefs = fs.readFileSync('schema.graphql', { encoding: 'UTF-8' });
+
+let id = 0;
+const photos = [];
+const users = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
-  }
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const { input } = args;
+      console.log(input);
+      const newPhoto = {
+        id: id++,
+        url: input.url,
+        name: input.name,
+        description: input.description,
+        category: input.photoCategory || 'PORTRAIT',
+        created: new Date(),
+      };
+      photos.push(newPhoto);
+      return newPhoto;
+    }
+  },
+  Photo: {
+    url: parent => `https://example.com/img/${parent.id}.jpg`,
+    postedBy: parent => users.find(u => u.githubLogin === parent.githubUser),
+    taggedUsers: parent => tags.filter(t => t.photoID === parent.id)
+      .map(t => t.userID)
+      .map(id => users.find(u => u.githubLogin === id))
+  },
+  User: {
+    postedPhotos: parent => photos.filter(p => p.githubUser === parent.githubLogin),
+    inPhotos: parent => tags.filter(t => t.userID === parent.id)
+      .map(t => t.photoID)
+      .map(id => photos.find(p => p.id === id)),
+  },
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A datetime value',
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value,
+  }),
 };
 
 const server = new ApolloServer({
