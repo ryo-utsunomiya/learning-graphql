d6302eec475d258a9025488938904d0d68316bbf
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-11-24 15:50:41 +0900

    add mongodb

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ const fs = require('fs');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const graphqlPlayground = require('graphql-playground-middleware-express').default;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const resolvers = require('./resolvers');
 
 const typeDefs = fs.readFileSync('schema.graphql', { encoding: 'UTF-8' });
 
-const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-server.applyMiddleware({app});
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
 
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
-app.get('/playground', graphqlPlayground({ endpoint: server.graphqlPath }));
+  const client = await MongoClient.connect(MONGO_DB, { useUnifiedTopology: true });
+  const db = client.db();
+  const context = { db };
 
-app.listen({port:4000}, () => {
-  console.log(`GraphQL Server running at http://localhost:4000${server.graphqlPath}`);
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+  });
+  server.applyMiddleware({ app });
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+  app.get('/playground', graphqlPlayground({ endpoint: server.graphqlPath }));
+
+  app.listen({ port: 4000 }, () => {
+    console.log(`GraphQL Server running at http://localhost:4000${server.graphqlPath}`);
+  });
+}
+
+start();
